528c739437d87625d2012a694198728337346696
Rimantas <belauzas@yahoo.com>
2019-06-03 20:58:42 +0300

    Kintamuju iniciavimas

---
+++ js/main.js
@@ -0,0 +1,42 @@
+"use strict";
+
+// kintamuju iniciavijas
+var a = 1;
+console.log( a );
+
+var b = 2;
+console.log( b );
+
+var pomidoras_raudonas = 3.14;
+console.log( pomidoras_raudonas );
+
+
+var vardas = 'Rimantas';
+console.log( vardas );
+
+var zodis = "Vardo 'Jaronimas' kilme yra....";
+console.log( zodis );
+
+var sakinys = "Mano 'cipliarkoje' (lt. siltnamyje) auga pomidoru veisle \"Babushka\".";
+console.log( sakinys );
+
+var kabutes = 'Zodis "kabutese" gali buti ir \'taip\' arba va.';
+console.log(kabutes);
+
+
+var sarasas = [1, 2, 3];
+console.log( sarasas );
+
+var abecele = ['a', 'b', 'c', 'd'];
+console.log(abecele);
+
+var tuscias = [];
+console.log(tuscias);
+
+var miksas = [1, 'asd', [a, 5, 'wer'], 855, 'asdfasd'];
+console.log(miksas);
+
+var skaiciu_sarasas = [a, b, pomidoras_raudonas];
+console.log(skaiciu_sarasas);
+
+console.log( a+pomidoras_raudonas );
